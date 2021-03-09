2a9189c92e501abc57f16f64ffd96229a192b2e1
zhoushanbao1994 <zhoushanbao@126.com>
2021-03-09 14:09:57 +0800

    两种写入、两种读取方式

--- QXlsx_Example/main.cpp
+++ QXlsx_Example/main.cpp
@@ -1,6 +1,7 @@
 #include "dialog.h"
 
 #include <QApplication>
+#include <QDebug>
 #include "xlsxdocument.h"
 #include "xlsxchartsheet.h"
 #include "xlsxcellrange.h"
@@ -15,7 +16,12 @@ int main(int argc, char *argv[])
 
     QXlsx::Document xlsx;
     xlsx.write("A1", "Hello Qt!");  // write "Hello Qt!" to cell(A,1). it's shared string.
+    xlsx.write(2, 1, "Hello World!");
     xlsx.saveAs("Test.xlsx");       // save the document as 'Test.xlsx'
+    QVariant data = xlsx.read("A1");
+    qDebug() << data.type() << data.toString();
+    data = xlsx.read(2, 1);
+    qDebug() << data.type() << data.toString();
 
     Dialog w;
     w.show();
